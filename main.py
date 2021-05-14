774b2aa6096e1f57ad50ac315966a04870aeb94d
OmarMo7 <omar7.first@gmail.com>
2021-05-14 16:33:13 +0200

    Importing data with extracting images&labels

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+from mlxtend.data import loadlocal_mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+
+def ExtractAndReshape(imagesPath, labelsPath):
+    images, images_labels = loadlocal_mnist(
+        images_path=imagesPath,
+        labels_path=labelsPath)
+    return images, images_labels
+
+
+train_images, train_labels = ExtractAndReshape(
+    "train-images.idx3-ubyte", "train-labels.idx1-ubyte")
+test_images, test_labels = ExtractAndReshape(
+    "t10k-images.idx3-ubyte", "t10k-labels.idx1-ubyte")
