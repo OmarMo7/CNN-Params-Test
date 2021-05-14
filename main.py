1f9d1312e643dc0f9ed7c9ca17023796260c2df1
OmarMo7 <omar7.first@gmail.com>
2021-05-14 16:37:49 +0200

    Creating the basic params & the model structure

--- main.py
+++ main.py
@@ -26,3 +26,18 @@ test_images = (test_images / 255) - 0.5
 # Reshape the images.
 train_images = np.expand_dims(train_images, axis=3)
 test_images = np.expand_dims(test_images, axis=3)
+
+
+# Basic parameters
+num_filters = 8
+filter_size = 3
+pool_size = 2
+
+
+# Build the model.
+model = Sequential([
+    Conv2D(num_filters, filter_size, input_shape=(28, 28, 1)),
+    MaxPooling2D(pool_size=pool_size),
+    Flatten(2),
+    Dense(10, activation='softmax'),
+])
